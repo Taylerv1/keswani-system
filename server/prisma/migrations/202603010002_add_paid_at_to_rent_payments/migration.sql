923e4b4249b2913513a65fba0e3ab1d5e656c203
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-03-02 13:46:10 +0200

    refactor(rent): remove paid_at and manual_receipt_ref columns from rent_payments migration

--- server/prisma/migrations/202603010002_add_paid_at_to_rent_payments/migration.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "rent_payments"
-ADD COLUMN "paid_at" DATE;
-
--- Backfill: records that are already settled can use current payment_date
--- as a best-effort historical paid date.
-UPDATE "rent_payments"
-SET "paid_at" = "payment_date"
-WHERE "paid_at" IS NULL
-  AND "status" IN ('paid', 'partial');
--- server/prisma/migrations/202603010003_add_manual_receipt_ref_to_rent_payments/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "rent_payments"
-ADD COLUMN "manual_receipt_ref" TEXT;
