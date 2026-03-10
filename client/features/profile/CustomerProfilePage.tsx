25561e2c078a421fede2ae32f6b0c1da8164ae26
Taylerv1 <mhmodshhade230@gmail.com>
2026-03-10 07:18:01 +0200

    simplify CustomerProfilePage layout and enhance CustomerProfileCard styling

--- client/features/profile/CustomerProfilePage.tsx
+++ client/features/profile/CustomerProfilePage.tsx
@@ -19,11 +19,7 @@ export default function CustomerProfilePage() {
                 </p>
             </div>
 
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="py-6 sm:py-10">
-                    <CustomerProfileCard user={data.user} />
-                </div>
-            </div>
+            <CustomerProfileCard user={data.user} />
         </div>
     );
 }
